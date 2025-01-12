ba6c7613c54dbd7984585840eb662e54055be5c1
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-01-12 21:46:51 +0600

    convert celsius to fahrenheit

---
+++ Basic_Algorithm_Scripting/1.js
@@ -0,0 +1,9 @@
+// Convert Celsius to Fahrenheit 💛💚💛
+
+function convertCtoF(celsius) {
+    let fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit;
+  }
+  
+ let result = convertCtoF(30);
+ console.log(result);
