6c1ff34522eab34ebfe7f9bcef5821fe1fb129e5
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-01-23 22:40:43 +0600

    title case a sentence problem solved

--- Basic_Algorithm_Scripting/11.title-case-a-sentence.js
+++ Basic_Algorithm_Scripting/11.title-case-a-sentence.js
@@ -1,12 +1,14 @@
 // title case a sentence 💛💚💛
 
 function titleCase(str) {
-    let text = str.split(' ')
-    for(let i = 0; i < text.length; i++){
-        console.log(text[i][0].toUpperCase());;
-    }
-    return text;
+  let textStr = str.split(' ');
+  for(let i = 0; i < textStr.length; i++){
+    textStr[i] = textStr[i].toLowerCase().split('');
+    textStr[i][0] = textStr[i][0].toUpperCase();
+    textStr[i] = textStr[i].join('');
   }
-  
+  return textStr.join(' ')
+}
+ 
 const result = titleCase("I'm a little tea pot");
 console.log(result);
