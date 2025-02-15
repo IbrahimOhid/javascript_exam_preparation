fafc86f045c3bc7c26e58b0159a1cb817d6139b0
Mohammad Ibrahim <ibrahimohid77@gmail.com>
2025-02-15 11:18:05 +0600

    Where do I Belong solved

--- Basic_Algorithm_Scripting/14.where_do_I_belong.js
+++ Basic_Algorithm_Scripting/14.where_do_I_belong.js
@@ -1,29 +1,14 @@
 // Where do I Belong 💛💚💛
 
-// function getIndexToIns(arr, num) {
-//     for(let i = 0; i < arr.length; i++){
-//         console.log(arr[i]);
-//         if(num > arr[i]){
-//             console.log(arr);
-//             return arr.sort(arr[i])
-//         }
-        
-//     }
-    
-//   }
-  
-//  const result = getIndexToIns([40, 60], 50);
-//  console.log(result);
-
 function getIndexToIns(arr, num) {
-    // Find my place in this sorted array.
-    var lol=0;
-    for(var i =0 ; i<arr.length; i++){
-      if(arr[i]<num){ lol++; }
+    let storeNum = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] < num){
+            storeNum++;
+        } 
     }
-    return lol;
+    return storeNum;
   }
   
- const result = getIndexToIns([10, 20, 30, 40, 50], 30);
+ const result = getIndexToIns([10, 20, 30, 40, 50], 35) ;
  console.log(result);
-  
